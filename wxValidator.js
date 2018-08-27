bad6fe3d79a77065b211899393144a660ec25ac9
diamondloler <857276958@qq.com>
2018-08-27 10:27:49 +0800

    g

--- wxValidator.js
+++ wxValidator.js
@@ -81,7 +81,9 @@
               //获得每个验证规则的结果
               flag = this.getCheckFunc(singleRule)(value)
             } catch (e) {
-
+              //移除未注册的规则
+              ruleList.splice(i, 1)
+              
               //如果添加了未定义规则，跳过这次循环。
               continue;
             }
