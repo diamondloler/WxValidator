13ade7a98c1b5bbf16e9902183c2b0f2d1af40a5
diamondloler <857276958@qq.com>
2018-11-02 11:36:09 +0800

    Update wxValidator.js

--- wxValidator.js
+++ wxValidator.js
@@ -40,7 +40,7 @@
           return !(/^\s*$/).test(val)
       },
       'phone': function (val) {
-          return /[0-9]{11}/.test(val)
+          return /^[0-9]{11}$/.test(val)
       },
       'date': function (val) {
           return /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/.test(val)
